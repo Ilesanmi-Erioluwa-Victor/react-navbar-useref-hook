5f06f8743f8d83a40de595942a9fbd14ea1c38fe
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-04 13:56:13 +0100

    Created my Data file

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./Data";
 
 const Navbar = () => {
   return <div>Navbar</div>;
