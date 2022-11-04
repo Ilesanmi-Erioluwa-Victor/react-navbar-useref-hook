622cb5a37e11592416faba352faf35d46c2dd43d
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-04 14:21:05 +0100

    Working on my Header

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,10 @@ const Navbar = () => {
     <nav>
       <div className="nav-center">
         <div className="nav-header"></div>
+
         <div className="links-container show-container"></div>
+
+        <ul className="social-icons"></ul>
       </div>
     </nav>
   );
