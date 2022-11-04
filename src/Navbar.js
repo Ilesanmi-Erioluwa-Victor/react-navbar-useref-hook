65730794bbceb2374af0942bc30c1955f46fbbf8
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-04 15:07:18 +0100

    Added my hamburger functionalities

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,18 +3,24 @@ import { FaBars, FaTwitter } from "react-icons/fa";
 import { links, social } from "./Data";
 
 const Navbar = () => {
+  const [showList, setShowList] = useState(false);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <h3>Logo</h3>
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={() => setShowList(!showList)}>
             {" "}
             <FaBars />{" "}
           </button>
         </div>
 
-        <div className="links-container show-container">
+        <div
+          className={`${
+            showList ? "links-container show-container" : "links-container"
+          }`}
+        >
           <ul className="links">
             {links.map((link) => {
               const { id, text, url } = link;
