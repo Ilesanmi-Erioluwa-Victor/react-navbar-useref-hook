40c5db914d9ddd667c343db99c5c608cc2cc7353
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-04 14:15:11 +0100

    Wotking on header

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,14 @@ import { FaBars, FaTwitter } from "react-icons/fa";
 import { links, social } from "./Data";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header"></div>
+        <div className="links-container show-container"></div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
